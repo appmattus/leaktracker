ce99af72dbc620c889ea5667c28123e31135dd41
Matthew Dolan <info@mattdolan.com>
2019-04-20 22:34:05 +0100

    Update to latest Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,23 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.3.0'
+    extra["kotlin_version"] = "1.3.30"
 
     repositories {
         jcenter()
     }
     dependencies {
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
-        classpath 'org.kt3k.gradle.plugin:coveralls-gradle-plugin:2.8.2'
-        classpath 'com.novoda:bintray-release:0.8.1'
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${extra["kotlin_version"]}")
+        classpath("org.kt3k.gradle.plugin:coveralls-gradle-plugin:2.8.2")
+        classpath("com.novoda:bintray-release:0.9")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
+}
+
+plugins {
+    id("com.github.ben-manes.versions") version "0.21.0"
 }
 
 allprojects {
@@ -24,7 +28,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    //noinspection GroovyAssignabilityCheck
-    delete rootProject.buildDir
+task("clean", type = Delete::class) {
+    delete(rootProject.buildDir)
 }
